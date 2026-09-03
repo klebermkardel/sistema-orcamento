9c671d4faf72deeedfc417abe4022f91b9f564c0
klebermkardel <molinakardel@outlook.com.br>
2026-09-03 10:29:27 -0300

    chore: instala e configura o pacote CORS no backend para permitir requisicoes do frontend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { open } = require('sqlite');
 const sqlite3 = require('sqlite3');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 let db;
 
